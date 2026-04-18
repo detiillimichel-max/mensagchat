a425141241999af427fd42dd0f714daeacff212f
Michel Detiilli <detiillimichel@gmail.com>
2026-04-17 23:50:12 -0300

    Add database service with Supabase client

---
+++ services/database.js
@@ -0,0 +1,4 @@
+// services/database.js
+const SB_URL = 'https://uqdwtzlkqaosnweyoyit.supabase.co';
+const SB_KEY = 'sb_publishable_uafBQD1aJ3w8_eq4meOsNQ_wzk8TwhA';
+const sb = supabase.createClient(SB_URL, SB_KEY);
